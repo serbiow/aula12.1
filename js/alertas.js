0a2535210aa5dfd625e32ffa45f046a887c27a61
serbiow <sergiohenriqueqqq@gmail.com>
2023-11-18 10:49:06 -0300

    terceiro commit

---
+++ js/alertas.js
@@ -0,0 +1,12 @@
+function AlertaSimples() {
+    alert("Olá")
+}
+
+function AlertaConfirma() {
+    confirm("Você deseja confirmar?")
+}
+
+function AlertaEntrada() {
+    let nome = prompt("Qual o seu nome?")
+    alert("O nome é: " + nome)
+}
